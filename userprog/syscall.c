eda542bb7bea78c6c17289128581f2c90245ff1b
wuyifat <wuyifat@yeah.net>
2014-04-24 22:08:53 -0700

    Update syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -4,7 +4,43 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+#define MAX_ARGS 3
+
+static void syscall_handler (struct intr_frame *f UNUSED)
+{
+  int arg[MAX_ARGS];
+  int esp = getpage_prt((const void v)f->esp);
+  switch (*(int *)esp)
+  case SYS_HALT
+  {
+    halt();
+    break;
+  }
+  case SYS_EXEC
+  {
+    get_args(f, arg, 1);
+    check_string((const void *) argv[0]);
+    f->eax = exec((const void *)argv[0]);
+    break;
+  }
+}
+
+pid_t exec(const char * cmd_line)
+{
+  pid_t pid = process_execute(cmd_line);
+  struct child_process *cp = get_child_process(pid);
+  if (!cp) {return ERROR;}
+  if (cp->load == NOT_LOADED)
+  {
+    sema_down(&cp->load_sema);
+  }
+  if (ep->load == LOAD_FAIL)
+  {
+    remove_cp(cp);
+    return ERROR;
+  }
+  return pid;
+}
 
 void
 syscall_init (void) 
