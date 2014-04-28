91d942ad2402baed075efa09b79331cf8ac6ec00
wuyifat <wuyifat@yeah.net>
2014-04-27 17:16:40 -0700

    Update syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,8 +3,11 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 #define MAX_ARGS 3
+
+struct lock *syslock;
 
 static void syscall_handler (struct intr_frame *f UNUSED)
 {
@@ -34,12 +37,53 @@ pid_t exec(const char* cmd_line)
   {
     sema_down(&cp->load_sema);
   }
-  if (ep->load == LOAD_FAIL)
+  if (cp->load == LOAD_FAIL)
   {
     remove_cp(cp);
     return ERROR;
   }
   return pid;
+}
+
+void halt(void)
+{
+  shutdown_power_off();
+}
+
+void exit(int status)
+{
+  struct thread *cur_thread = thread_current();
+  if (thread_alive(cur_thread -> parent))
+  {
+    cur_thread -> parent -> status = status; //????????????????
+  }
+  thread_exit();
+}
+
+int wait(pid_t pid)
+{
+  return process_wait(pid);
+}
+
+bool create(const char* file, unsigned initial_size)
+{
+  lock_acquire(syslock);
+  bool success = filesys_create(file, initial_size);
+  lock_release(syslock);
+  return success;
+}
+
+bool remove(const char* file)
+{
+  lock_acquire(syslock);
+  bool success = filesys_remove(file);
+  lock_release(syslock);
+  return success;
+}
+
+int open(const char* file)
+{
+  
 }
 
 void
