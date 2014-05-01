6bccbd66ffa1f461af42c8fb51f1d65473251f00
wuyifat <wuyifat@yeah.net>
2014-05-01 15:25:23 -0700

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -61,7 +61,7 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (file_name, &if_.eip, &if_.esp, &saveptr);
   
   if(success) {thread_current()->cp->load = LOADED;}
   else {thread_current()->cp->load = LOAD_FAIL;}
