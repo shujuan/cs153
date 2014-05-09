f118fa2b57cc65446e1d3949d90c5e73f10c21be
shujuan <shujuan.wang11@gmail.com>
2014-05-09 16:22:08 -0700

    Update process.h

--- userprog/process.h
+++ userprog/process.h
@@ -4,8 +4,13 @@
 #include "threads/thread.h"
 
 tid_t process_execute (const char *file_name);
+static void start_process(void *file_name_);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+bool load(const char *file_name, void(**eip) (void), void **esp, char **saveptr);
+static bool load_segment(struct file *file, off_t ofs, uint8_t *upage, uint32_t read_bytes, uint32_t zero_bytes, bool writable);
+static bool setup_stack(void **esp, const char* file_name, char** save_ptr);
+
 
 #endif /* userprog/process.h */
