f51708e3f3c2578fb2dac442e4c3c828435d37a9
shujuan <shujuan.wang11@gmail.com>
2014-05-09 15:51:17 -0700

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -39,6 +39,9 @@ process_execute(const char *file_name)
 	if (fn_copy == NULL)
 		return TID_ERROR;
 	strlcpy(fn_copy, file_name, PGSIZE);
+	
+	char *saveptr;
+	file_name=strotok_r((char*)file_name, "", &saveptr);
 
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create(file_name, PRI_DEFAULT, start_process, fn_copy);
