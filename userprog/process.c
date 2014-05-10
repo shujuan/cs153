84faa4b514f7601b0b8f63e82aa1240a909d1396
shujuan <shujuan.wang11@gmail.com>
2014-05-09 17:31:40 -0700

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -44,7 +44,7 @@ process_execute(const char *file_name)
 	strlcpy(fn_copy, file_name, PGSIZE);
 	
 	char *saveptr;
-	file_name=strotok_r((char*)file_name, "", &saveptr);
+	file_name=strtok_r((char*)file_name, "", &saveptr);
 
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create(file_name, PRI_DEFAULT, start_process, fn_copy);
